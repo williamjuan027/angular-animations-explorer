792a33223fdc1c171a316e11e827d4ea7e4017b0
Chau Tran <nartc7789@gmail.com>
2021-04-25 19:31:46 -0500

    state-change-demo with popmotion

--- src/app/content/basic/state-change/state-change.demo.module.ts
+++ src/app/content/basic/state-change/state-change.demo.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StateChangeDemoComponent } from './state-change.demo.component';
-import { ContainersModule } from '@shared/components/containers';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ButtonsModule } from '@shared/components/buttons';
+import { ContainersModule } from '@shared/components/containers';
+import { StateChangePopmotionDemoDirective } from './state-change-popmotion-demo.directive';
+import { StateChangeDemoComponent } from './state-change.demo.component';
 
 @NgModule({
-  declarations: [StateChangeDemoComponent],
+  declarations: [StateChangeDemoComponent, StateChangePopmotionDemoDirective],
   exports: [StateChangeDemoComponent],
   imports: [CommonModule, FlexLayoutModule, ContainersModule, ButtonsModule],
 })
