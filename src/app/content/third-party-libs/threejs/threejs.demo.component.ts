de8a92c6778ba57b71cebeca08c0000abf6295db
William Juan <williamjuan027@gmail.com>
2021-06-18 12:37:19 +0700

    remove three

--- src/app/content/third-party-libs/threejs/threejs.demo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-threejs-demo',
-  templateUrl: './threejs.demo.component.html',
-  styleUrls: ['./threejs.demo.component.scss'],
-})
-export class ThreejsDemoComponent {}
