605c2019a6e6eb473a98d987ec50b0369298d540
William Juan <williamjuan027@gmail.com>
2021-06-18 12:23:07 +0700

    add threejs section

---
+++ src/app/content/third-party-libs/threejs/threejs.demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-threejs-demo',
+  templateUrl: './threejs.demo.component.html',
+  styleUrls: ['./threejs.demo.component.scss'],
+})
+export class ThreejsDemoComponent {}
