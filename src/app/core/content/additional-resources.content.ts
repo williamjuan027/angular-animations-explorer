8a6f22d9d432e39ad7e07c8c5123b44a675d7237
william <williamjuan027@gmail.com>
2023-03-15 16:05:35 +0800

    add angular animation course to resource list

--- src/app/core/content/additional-resources.content.ts
+++ src/app/core/content/additional-resources.content.ts
@@ -2,6 +2,11 @@ import { AdditionalResource } from "../models";
 
 export const AdditionalResources: AdditionalResource[] = [
     {
+        title: 'Animations in Angular Course',
+        description: 'This course will provide you with information and advice to create beautiful and performant animations in Angular applications. The course covers everything animation-related, from choosing the right tools for the jobs to debugging and profiling your animations.',
+        url: 'https://www.educative.io/courses/animations-in-angular'
+    },
+    {
         title: 'Get Better Angular UX Using 4 Animation Techniques',
         description: 'Improve the User Experience (UX) of an e-commerce application by implementing animations utilizing Angular animations, CSS animations, and Web Animation APIs.',
         url: 'https://auth0.com/blog/get-better-angular-ux-using-animation-techniques/'
