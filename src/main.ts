ab39d12526d8b907d78df931b8246806c417fc39
william <williamjuan027@gmail.com>
2023-06-20 10:01:27 +0800

    feat: add lazy loaded animations

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { ROUTES } from './app/routes';
 import { MarkdownModule } from 'ngx-markdown';
+import { provideLazyLoadedAnimations } from '@angular/platform-browser/lazy-animations';
 
 bootstrapApplication(AppComponent, {
   providers:[
+    provideLazyLoadedAnimations(),
     importProvidersFrom(
       RouterModule.forRoot(ROUTES),
       HttpClientModule,
-      BrowserAnimationsModule,
+      // BrowserAnimationsModule,
       MarkdownModule.forRoot(),
     ),
   ]
