1d0000e673b29190b03c3106ff3aaa380ffd58c0
william <williamjuan027@gmail.com>
2025-01-12 09:36:08 +0800

    feat: ng 18 update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {  } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { ROUTES } from './app/routes';
