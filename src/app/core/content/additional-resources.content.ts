270d3c6495e861657a54c93e8e411259746eb437
william <williamjuan027@gmail.com>
2025-05-09 11:35:16 +0800

    feat: add more resources

--- src/app/core/content/additional-resources.content.ts
+++ src/app/core/content/additional-resources.content.ts
@@ -2,6 +2,11 @@ import { AdditionalResource } from "../models";
 
 export const AdditionalResources: AdditionalResource[] = [
     {
+        title: 'Animations in Angular eBook',
+        description: 'Master the art of bringing your Angular applications to life! Animations in Angular is your comprehensive guide to adding a wide range of dynamic animations, from subtle transitions to complex sequences. Enhance user experience and create visually stunning interfaces with this practical, hands-on resource.',
+        url: 'https://pointarctic.lemonsqueezy.com/buy/5ea8d65f-6ff3-4a86-8849-cee82a80ab03'
+    },
+    {
         title: 'Animations in Angular Course',
         description: 'This course will provide you with information and advice to create beautiful and performant animations in Angular applications. The course covers everything animation-related, from choosing the right tools for the jobs to debugging and profiling your animations.',
         url: 'https://www.educative.io/courses/animations-in-angular'
